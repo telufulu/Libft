a0df1dff3098a77d7a98f7ee78bdd5dc2257b5f7
tlufulua <tlufulua@student.42madrid.com>
2023-08-01 20:52:57 +0200

    adding test

---
+++ .test/main.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 18:31:57 by telufulu          #+#    #+#             */
+/*   Updated: 2023/08/01 20:51:01 by telufulu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (!strcmp(argv[1], "ft_isalpha") || !strcmp(argv[1],"all"))
+		{
+			char	a = 'A';
+			char	b = 'z';
+			char	c = -90;
+			
+			printf("org: %i\n cpy: %i\n", isalpha(a), ft_isalpha(a));
+			printf("org: %i\n cpy: %i\n", isalpha(b), ft_isalpha(b));
+			printf("org: %i\n cpy: %i\n", isalpha(c), ft_isalpha(c));
+			if (isalpha(a) == ft_isalpha(a) &&
+				isalpha(b) == ft_isalpha(b) &&
+				isalpha(c) == ft_isalpha(c))
+				printf("FT_ISALPHA \x1b[32mOK\x1b[0m\n");
+			else	
+				printf("FT_ISALPHA \x1b[31mKO\x1b[0m\n");
+		}
+		if (!strcmp(argv[1], "ft_isdigit") || !strcmp(argv[1],"all"))
+		{
+			char	a = '9';
+			char	b = -1;
+			char	c = 90;
+			
+			printf("org: %i\n cpy: %i\n", isdigit(a), ft_isdigit(a));
+			printf("org: %i\n cpy: %i\n", isdigit(b), ft_isdigit(b));
+			printf("org: %i\n cpy: %i\n", isdigit(c), ft_isdigit(c));
+			if (isdigit(a) == ft_isdigit(a) &&
+				isdigit(b) == ft_isdigit(b) &&
+				isdigit(c) == ft_isdigit(c))
+				printf("FT_ISDIGIT \x1b[32mOK\x1b[0m\n");
+			else	
+				printf("FT_ISDIGIT \x1b[31mKO\x1b[0m\n");
+		}
+	}
+	else
+		printf("\x1b[31merror: \x1b[0m empty argument\n");
+	return (0);
+}
---
+++ .test/test.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+make re -C ../ 
+#gcc -Wall -Werror -Wextra -fsanitize=address -g3 main.c -I ../inc/ -L .. -lft
+gcc -Wall -Werror -Wextra main.c -I ../inc/ -L .. -lft
+./a.out $1 && rm -rf a.out*
