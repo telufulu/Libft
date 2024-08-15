60c7466b158b1e2089bfa6b75ad0134562b22364
Teresa Bulungu Lufuluabo Pastor <telufulu@Teresas-MacBook-Pro.local>
2024-08-15 14:49:33 +0200

    print_matrix.c

--- srcs/print_matrix.c
+++ srcs/print_matrix.c
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 14:43:44 by telufulu          #+#    #+#             */
-/*   Updated: 2024/08/15 14:45:08 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:48:53 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	print_matrix(char **matrix, int fd)
 	while (matrix && *matrix)
 	{
 		ft_putstr_fd(*matrix, fd);
+		write(1, "\n", 1);
 		matrix++;
 	}
 }
