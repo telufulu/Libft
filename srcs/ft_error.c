1ce392e55ca23541c97cd8cf1b9b638bf1011112
Teresa B. Lufuluabo Pastor <teresa@teresalufuluabo.es>
2024-09-21 17:56:27 +0200

    better ft_error

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 22:00:42 by telufulu          #+#    #+#             */
-/*   Updated: 2024/09/01 20:47:27 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/09/21 17:44:48 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,12 @@ void	ft_error(char *err, char *str_error)
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(DEFAULT, 2);
 	if (err && *err)
-	{
 		ft_putstr_fd(err, 2);
-		ft_putstr_fd("\n", 2);
-	}
 	if (str_error)
 	{
 		ft_putstr_fd(" - ", 2);
 		ft_putstr_fd(str_error, 2);
-		ft_putstr_fd("\n", 2);
 	}
+	ft_putstr_fd("\n", 2);
 	exit(EXIT_FAILURE);
 }
