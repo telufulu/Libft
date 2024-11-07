678cda8bf991686dfdaa7fe94e3c4061ccef28df
Teresa B. Lufuluabo Pastor <teresa@teresalufuluabo.es>
2024-11-07 13:03:39 +0100

    fixed print_matrix

--- srcs/ft_print_matrix.c
+++ srcs/ft_print_matrix.c
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 14:43:44 by telufulu          #+#    #+#             */
-/*   Updated: 2024/09/01 17:48:18 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/11/06 17:26:40 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_print_matrix(char **matrix, int fd)
 {
-	while (matrix && *matrix)
+	while (matrix && *matrix && **matrix)
 	{
 		ft_putstr_fd(*matrix, fd);
 		write(1, "\n", 1);
